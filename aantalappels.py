aa74e178a17b564b2b2c162b053ea7abffafa0c2
jesseboon04 <boonjesse04@gmail.com>
2020-10-23 12:39:12 +0200

    aantalappels opdracht toegevoegd

---
+++ aantalappels.py
@@ -0,0 +1,24 @@
+import math
+
+#Vervang de 0-en met de juiste berekeningen
+
+trees               = 333             #hoeveel bomen zijn er in totaal?
+shadedTrees         = math.ceil((trees/3)*2)  #hoeveel bomen staan er in de schaduw (afgerond naar boven)?
+sunnyTrees          = trees-shadedTrees            #hoeveel in de zon?
+
+shadeOutputModifier = 80            #hoeveel procent productie geeft een schaduwboom?
+
+sunnyTreeOutput     = 146             #hoeveel appels geeft 1 zonnige boom?
+shadedTreeOutput    = (sunnyTreeOutput/100)*shadeOutputModifier            #hoeveel appels geeft 1 schaduw boom?
+
+sunnyOutput         = sunnyTreeOutput*sunnyTrees             #hoeveel appels geven alle zonnige bomen? 
+shadedOutput        = shadedTrees*shadedTreeOutput          #hoeveel appels geven alle schaduw bomen?
+totalOutput         = sunnyOutput+shadedOutput          #hoeveel appels zijn er in totaal?
+
+owners              = 3             #met hoeveel mensen verdelen we de appels?
+
+eatCount            = totalOutput % owners          #hoeveel appels houden we over omdat ze niet eerlijk te verdelen zijn?
+sellableOutput      = totalOutput-eatCount            #hoeveel appels zijn er over en dus verkoopbaar?
+cut                 = sellableOutput/3             #hoeveel appels mag ik verkopen?
+
+print(cut)
